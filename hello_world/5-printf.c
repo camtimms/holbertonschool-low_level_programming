41e34f2fcccab32eeda8725eec4695a73cc00932
camtimms <cam.g.timms@gmail.com>
2025-02-17 04:52:46 +0000

    fixed description 5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints using function printf
+ *
+ * Return: end function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+	return (0);
+}
