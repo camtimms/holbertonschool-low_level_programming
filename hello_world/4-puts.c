e8197d717c7833459035d2e0cdd27398ca777674
camtimms <cam.g.timms@gmail.com>
2025-02-17 04:39:59 +0000

    added a description to the file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints using puts function.
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	char *str1  = "\"Programming is like building a multilingual puzzle";
