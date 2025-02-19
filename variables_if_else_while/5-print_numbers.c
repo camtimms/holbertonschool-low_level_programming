e1243d99f0750b39b45fbcec0913ddf694773a9f
camtimms <cam.g.timms@gmail.com>
2025-02-19 10:57:04 +0000

    added 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the base 10 digits
+ *
+ * Description: prints the base 10 digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* Declare char variable */
+	char ch = '1';
+
+	for (ch = '1'; ch <= '9'; ch++)
+		putchar(ch);
+	return (0);
+}
