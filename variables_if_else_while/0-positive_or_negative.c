9107ba6f7774fbb6878c8a403f801ef01ac0cae4
camtimms <cam.g.timms@gmail.com>
2025-02-19 09:04:08 +0000

    woo, finished my first c code. 0-positive_or_negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - checks if number is positive or negative
+ * @n: random number
+ *
+ * Description: Uses if, elif and else statements to check if
+ * the number is positive or negative or 0.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					if (n > 0)
+					{
+						printf("%d is positive\n", n);
+					}
+					else if (n < 0)
+					{
+						printf("%d is negative\n", n);
+					}
+					else
+					{
+						printf("%d is zero\n", n);
+					}
+					return (0);
+}
