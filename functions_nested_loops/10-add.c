538413fec10dcae55f346fc873caab0609ee2ef3
camtimms <cam.g.timms@gmail.com>
2025-02-21 22:51:03 +0000

    added 10-add.c lol

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: int 1
+ * @b: int 2
+ * Description: adds two numbers
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
