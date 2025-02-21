9d6dba7fde2d8d4c4e9209048a9d07e70181d626
camtimms <cam.g.timms@gmail.com>
2025-02-21 20:15:02 +0000

    finnally completed 9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ *
+ * Description: prints the times tables up to 9
+ * Return: void
+ */
+void times_table(void)
+{
+	int n; /* time table factor */
+	int x; /* multiplier */
+	int result; /* stores the result of n * x */
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			result = n * x;
+			if (x == 9)
+			{	
+				if (result < 10)
+				{	
+					_putchar(' ');
+					_putchar('0' + result);
+					_putchar('$');
+					_putchar('\n');
+				}
+				else if (result < 100)
+				{
+					_putchar('0' + result / 10);
+					_putchar('0' + result % 10);
+					_putchar('$');
+					_putchar('\n');
+				}
+			}
+			else if (result < 10)
+			{
+				_putchar('0' + result);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (result < 100)
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
