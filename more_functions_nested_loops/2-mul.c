fdff474c9c1f426daac2e4d6bd0e746722808fb6
camtimms <cam.g.timms@gmail.com>
2025-02-24 00:02:27 +0000

    added 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Description: Multiplies two numbers
+ * Return: Multiplication result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
