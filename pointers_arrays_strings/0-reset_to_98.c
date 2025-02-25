d4276e7e568bc7c75ae014254d5a6fc7fa8080fa
camtimms <cam.g.timms@gmail.com>
2025-02-25 03:42:39 +0000

    started project: pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an int's value to 98
+ *
+ * @n: A pointer address
+ * Description: Uses a pointer to change the value of an int to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a dir for the code for learning about pointers and memory
