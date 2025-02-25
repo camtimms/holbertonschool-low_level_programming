ed7b24ddbda675d76f3b361a451008dd738d2764
camtimms <cam.g.timms@gmail.com>
2025-02-25 05:17:27 +0000

    added 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ *
+ * @s: A string
+ * Description: Prints a string in reverse by locating the last memory position
+ * and prints it backwards
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int n = 0;
+
+	/* Find location in memory */
+	while (*s)
+	{
+		s++;
+		n++;
+	}
+	/* n should be the lenth of the str */
+	while (n != 0)
+	{
+		_putchar(s[n]);
+		n--;
+	}
+	_putchar('\n');
+}
