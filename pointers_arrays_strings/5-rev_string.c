d39db56aff84ae86361ff698e4a376a30d523565
camtimms <cam.g.timms@gmail.com>
2025-02-26 20:42:08 +0000

    added 5-rev_string, although error:segmentatoin dump

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: String
+ * Description: Takes a string and reverses it in the memory.
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int start = 0;
+	int end;
+	char tmp;
+
+	/* Finds length of string */
+	while (s[end])
+	{
+		end++;
+	}
+	/* Remove null char */
+	end = end - 1;
+	/* Swap characters by rotating it through tmp */
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
+}
