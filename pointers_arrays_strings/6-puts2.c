3ca93bcf989049c57abd70fbaeff419e3bb215dd
camtimms <cam.g.timms@gmail.com>
2025-02-26 21:10:03 +0000

    checks every char for null

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,13 @@
 void puts2(char *str)
 {
 	int n = 0;
-	
+
 	/* Use index based approach to not jump past the null terminatot */
 	while (str[n] != '\0')
 	{
 		if (n % 2 == 0)
 			_putchar(str[n]);
-		
+
 		n++;
 	}
 	_putchar('\n');
