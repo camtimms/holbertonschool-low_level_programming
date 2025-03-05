75e8e5845ac13aa331ed178dd4659fb67880f2a8
camtimms <cam.g.timms@gmail.com>
2025-03-05 03:32:02 +0000

    fixed null edge case

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -15,9 +15,6 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	if (c == '\0')
-		return ('\0');
-
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
@@ -25,5 +22,9 @@ char *_strchr(char *s, char c)
 		i++;
 
 	}
+	
+	if (c == '\0')
+		return (&s[i]);
+
 	return ('\0');
 }
