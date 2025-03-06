1b5bafb31ddd3048c1df4bb15f5eb4afbec7f7d7
camtimms <cam.g.timms@gmail.com>
2025-03-06 05:13:11 +0000

    completed task 4

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -16,24 +16,25 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
 	int j = 0;
-	int prefix_length = 0;
+	unsigned int prefix_length = 0;
+	int match = 0;
 
 	while (s[i] != '\0')
 	{
 		j = 0;
-
+		match = 0;
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
+			{
 				prefix_length++;
-
-			else if (accept[j] == '\0')
-				return (prefix_length);
-
-			else
-				j++;
+				match = 1;
+			}
+			j++;
 		}
 
+		if (match == 0)
+			return (prefix_length);
 		i++;
 	}
 	return (prefix_length);
