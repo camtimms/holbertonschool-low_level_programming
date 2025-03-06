a9fd0d553801c28ef2a0e312b295c389a0169e1a
camtimms <cam.g.timms@gmail.com>
2025-03-06 13:06:48 +0000

    refactored code for 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strstr - Finds the location of a search string in a source string
@@ -15,30 +16,40 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int j = 0;
-	char *match;
+	int i = 0, ii; /* haystack cursors */
+	int j = 0, jj; /* needle cursors */
+	char *match; /* save start of location where a match has occoured */
+
+	if (needle[0] == '\0') /* If needle is empty, return haystack */
+	{
+		return (haystack);
+	}
 
 	while (haystack[i] != '\0')
 	{
 		j = 0;
-		while (needle[j] != '\0')
+		if (haystack[i] == needle[j]) /* Check for a match */
 		{
-			if (haystack[i] == needle[j])
-			{
-				match = &haystack[i];
+			match = &haystack[i]; /* Save address if a match is found */
+			ii = i; /* Set second cursors to the current positon */
+			jj = j;
 
-				while (haystack[i] == needle[j])
-				{
-						i++;
-						j++;
-						if (needle[j] == '\0')
-							return (match);
-				}
+			/* Check if each element matches and also each string doesn't terminate */
+			while (haystack[ii] == needle[jj] &&
+			       haystack[ii] != '\0' &&
+			       needle[jj]   != '\0')
+			{
+				ii++;
+				jj++;
 			}
-			j++;
+
+			/* If we reached the end of needle above then we have a complete match */
+			if (needle[jj] == '\0')
+			{
+				return (match);
+			}
 		}
-	i++;
+		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
