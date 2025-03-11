7fee47d0841af1154c6b205d422092c80ecfef7f
camtimms <cam.g.timms@gmail.com>
2025-03-11 05:25:29 +0000

    added 4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Free's the array created by the last function alloc_grid
+ *
+ * @grid: Array
+ * @height: Height of the array
+ *
+ * Description: Frees the memory from the array
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	while (height >= 0)
+	{
+		free(grid[height]);
+		height--;
+	}
+	free(grid);
+}
