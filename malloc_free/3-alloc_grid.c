993341ef9548622eda96b440035adb2d3f5cd64b
camtimms <cam.g.timms@gmail.com>
2025-03-11 04:19:45 +0000

    added 3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Creates a 2D array of intergers
+ *
+ * @width: Width of array
+ * @height: Height of array
+ *
+ * Description: Returns a pointer to a 2D array of intergers
+ *
+ * Return: Pointer () or failure (NULL)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j = 0; /* Iterators */
+	int **arr;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int *) * height);
+
+	if (arr == NULL)
+		return (NULL);
+
+	while (i < height)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+
+		if (arr[i]  == NULL)
+			return (NULL);
+		i++;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return(arr);
+}
