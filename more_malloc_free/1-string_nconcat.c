2a2cb2c9283591c6a06d532e48d71b8080662b94
camtimms <cam.g.timms@gmail.com>
2025-03-12 01:57:24 +0000

    fixed space for null terminator

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -65,7 +65,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= l2)
 		n = l2;
 
-	concat = malloc(l1 + n);
+	concat = malloc(l1 + n + 1);
 	if (concat == NULL)
 		return (NULL);
 
