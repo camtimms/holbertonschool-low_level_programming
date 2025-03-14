630700822082d5b3a5c9a3f2df1781b8e987000c
camtimms <cam.g.timms@gmail.com>
2025-03-14 03:55:32 +0000

    used typedef to change dog to dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,14 +10,15 @@
  *
  * Description: Storing details of a doggo
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 
 #endif
