647d06317a86ee190bd9bf9b8441e62ac9ac0b0f
camtimms <cam.g.timms@gmail.com>
2025-03-14 00:54:04 +0000

    added description to struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - Details of a dog
+ *
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Storing details of a doggo
+ */
 struct dog
 {
 	char *name;
