3a5e9da8ac0ff173c8967ff54ea169370630c0c5
camtimms <cam.g.timms@gmail.com>
2025-03-18 01:22:41 +0000

    added 2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches through an array and finds the first occurrence of an
+ * integer
+ *
+ * @array: Array to search through
+ * @size: Size of array
+ * @cmp: Comparision function that checks the number
+ *
+ * Description
+ *
+ * Return: Location of number in array (int), if no matches (-1) and if
+ * size <= 0 (-1).
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int flag;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		flag = cmp(array[i]);
+
+		if (flag != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
