1886458a6c6f5422835b65ddcb6bdb515320adba
camtimms <cam.g.timms@gmail.com>
2025-03-20 05:40:28 +0000

    completed the 3-print_all.c file

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -15,10 +15,38 @@
 
 void print_all(const char * const format, ...)
 {
+	va_list args;
+	char (*get_print_func_fp)(char *s);
+	int i = 0, j = 0;
 
+	va_start(args, format);
+
+	while (i < 4)
+	{
+		j = 0;
+
+		while (format[j] != '\0')
+		{
+			if (format[j] == get_print_func_fp[i].pf
+				get_print_func_fp[i].f(va_args);	
+			j++;
+		}
+	i++;
+
+	va_end(args);
 }
 
-void ((get_print_func_fp)(char *s))(char *s)
+/**
+ * get_print_func_fp - Function pointer to the right print function
+ *
+ * @s: String from format to compare
+ *
+ * Description: Function pointer to the right print function
+ *
+ * Return: void
+ */
+
+char (*get_print_func_fp)(char *s)
 {
 	print_funct pfs[] = {
 		{"c", print_char},
@@ -30,11 +58,77 @@ void ((get_print_func_fp)(char *s))(char *s)
 
 	int i = 0;
 
-	while (pfs[i] != NULL)
+	while (pfs[i].pf != NULL)
 	{
 		if (*s == *pfs[i].pf)
 			return (pfs[i].f);
 		i++;
 	}
-
+	return(pfs[i].f};
 }
+
+/**
+ * print_char - Prints a character
+ *
+ * @c: Character
+ *
+ * Description: Prints a character
+ *
+ * Return: void
+ */
+
+void print_char(char c);
+{
+	_putchar(c);
+}
+
+/**
+ * print_int - Prints an integer
+ *
+ * @s: String of int to print
+ *
+ * Description: Prints an integer
+ *
+ * Return: void
+ */
+
+void print_int(char *s)
+{
+	int n;
+	n = atoi(s)
+	printf("%d", n);
+}
+
+/**
+ * print_float - Prints a float
+ *
+ * @s: String of float to print
+ *
+ * Description: Prints a float
+ *
+ * Return: void
+ */
+
+void print_float(char *s)
+{
+	float n;
+	n = atof(s);
+	printf("%f", n);
+}
+
+/**
+ * print_str - Prints a string
+ *
+ * @s: String to print
+ *
+ * Description: Prints a string
+ *
+ * Return: void
+ */
+
+void print_str(char *s)
+{
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}	
