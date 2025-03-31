77564f78fa11c19ceb1e90e5ea583a5e99d42555
camtimms <cam.g.timms@gmail.com>
2025-03-31 05:35:27 +0000

    moved functions back to 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,6 +4,106 @@
 #include <stdlib.h>
 
 /**
+ * print_char - Prints a character
+ *
+ * @args: Variadic argument
+ *
+ * Description: Prints a character
+ *
+ * Return: void
+ */
+
+void print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	putchar(c);
+}
+
+/**
+ * print_int - Prints an integer
+ *
+ * @args: Variadic argument
+ *
+ * Description: Prints an integer
+ *
+ * Return: void
+ */
+
+void print_int(va_list args)
+{
+	int n = va_arg(args, int);
+
+	printf("%d", n);
+}
+
+/**
+ * print_float - Prints a float
+ *
+ * @args: Variadic argument
+ *
+ * Description: Prints a float
+ *
+ * Return: void
+ */
+
+void print_float(va_list args)
+{
+	double n = va_arg(args, double);
+
+	printf("%f", n);
+}
+
+/**
+ * print_str - Prints a string
+ *
+ * @args: Variadic argument
+ *
+ * Description: Prints a string
+ *
+ * Return: void
+ */
+
+void print_str(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+ * get_print_func_fp - Function pointer to the right print function
+ *
+ * @c: Character from format to compare
+ *
+ * Description: Function pointer to the right print function
+ *
+ * Return: print function
+ */
+
+void (*get_print_func_fp(char c))(va_list)
+{
+	print_func_t pfs[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_str},
+		{'\0', NULL}
+		};
+	int i = 0;
+
+	while (pfs[i].c != '\0')
+	{
+		if (c == pfs[i].c)
+			return (pfs[i].f);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
  * print_all - Prints the arguments based on the chars in the format
  *
  * @format: A string/set of chars which specify what data types to print
