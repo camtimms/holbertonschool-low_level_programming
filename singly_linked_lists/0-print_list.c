66d69a0b8d23c620957b18722cea39d5b859e637
camtimms <cam.g.timms@gmail.com>
2025-04-01 01:29:28 +0000

    fixed count and fixed betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,19 +1,29 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list - Prints the elements of a linked list
+ *
+ * @h: Head of the linked list
+ *
+ * Description: Prints the elements of a linked list
+ *
+ * Return: Length of the nodes in the list
+ */
+
 size_t print_list(const list_t *h)
 {
 	int count = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			count++;
-		}
+
 		h = h->next;
+		count++;
 	}
-	return (count);   
+	return (count);
 }
