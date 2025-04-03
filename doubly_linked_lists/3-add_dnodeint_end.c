d61e7371ad4a8ab8a9514def50be42dfbc5689e0
camtimms <cam.g.timms@gmail.com>
2025-04-03 06:07:44 +0000

    betty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,7 +7,7 @@
  * @head: Pointer to the first node in the doubly linked list
  * @n: Integer, data to store in list
  *
- * Description: Adds a node to the end of a doubly linked list 
+ * Description: Adds a node to the end of a doubly linked list
  *
  * Return: Address of new element (new_node) or fail (NULL)
  */
@@ -19,24 +19,23 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (!new_node)
 		return (NULL);
-	
+
 	/* Create new node */
-	new_node->prev = NULL; 
+	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
-	
+
 	if (*head == NULL)
 		return (*head = new_node);
-	
+
 	/* Find last node in list */
-	curr_node = *head; 
+	curr_node = *head;
 	while (curr_node->next != NULL)
 		curr_node = curr_node->next;
-	
-	/* Link nodes together */	
-	curr_node->next = new_node;
-	new_node->prev = curr_node; 
-	
 
-	return (new_node);	
+	/* Link nodes together */
+	curr_node->next = new_node;
+	new_node->prev = curr_node;
+
+	return (new_node);
 }
