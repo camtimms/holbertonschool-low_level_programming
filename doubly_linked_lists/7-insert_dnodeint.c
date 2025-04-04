1e215af1d2c04f50617c040c1e0260bf5915500f
camtimms <cam.g.timms@gmail.com>
2025-04-04 00:26:32 +0000

    shrunk code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -5,7 +5,7 @@
  * insert_dnodeint_at_index - Inserts node at specified index
  *
  * @h: Pointer to header of linked list
- * @inx: Index to place the new node
+ * @idx: Index to place the new node
  * @n: Integer data to place inside of new node
  *
  * Description: Inserts node at specified index
@@ -22,46 +22,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!new_node)
 		return (NULL);
-
 	/* Initialise node */
 	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
-
 	/* Special case idx = 0 */
 	if (idx == 0)
-	{
-		new_node->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
-
+		return (add_dnodeint(h, n));
 	/* Move to index */
 	while (i < idx && curr_node)
 	{
 		curr_node = curr_node->next;
 		i++;
 	}
-
-	
 	/* Check if curr_node is out of bounds */
 	if (i != idx)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	/* Set prev and next node */
 	prev_node = curr_node->prev;
-
 	/* Adjust pointers within nodes */
 	prev_node->next = new_node;
-
 	new_node->prev = prev_node;
 	new_node->next = curr_node;
-
 	curr_node->prev = new_node;
 
 	return (new_node);
