6c240eca197f907fc03e60c97d865d6ab8c29f8e
camtimms <cam.g.timms@gmail.com>
2025-04-04 00:02:43 +0000

    fixed case idx = 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -28,8 +28,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	/* Special case idx = 0 */
 	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
 		return (new_node);
+	}
 
 	/* Move to index */
 	while (i < idx)
