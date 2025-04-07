7f27d9cf57fc606e591553ba0c966b3c6969aec4
camtimms <cam.g.timms@gmail.com>
2025-04-07 07:01:10 +0000

    end of day

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_binary - Prints the binary of an int
+ *
+ * n: Integer
+ *
+ * Description
+ *
+ * Return: Void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int rem = 0;
+	int base = 1;
+
+	rem = n;
+	while (rem != 0)
+	{
+
+		base *= 2;
+	}
+}
