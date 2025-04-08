690de528ddef29dd1f7eacacf3d4ab5a7db138b6
camtimms <cam.g.timms@gmail.com>
2025-04-08 04:59:38 +0000

    added 1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -4,22 +4,35 @@
 /**
  * print_binary - Prints the binary of an int
  *
- * n: Integer
+ * @n: Integer
  *
- * Description
+ * Description: Uses bit opererations to run through the int n uusing the AND
+ * operator to check the least significant bit (LSB) and print it once it
+ * reaches the start of the number.
  *
  * Return: Void
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int rem = 0;
-	int base = 1;
+	int i = sizeof(unsigned long int) * 8 - 1;
+	int printed = 0;
 
-	rem = n;
-	while (rem != 0)
+	if (n == 0)
 	{
-
-		base *= 2;
+		_putchar('0');
+		return;
+	}
+	while (i >= 0)
+	{
+		/* and check */
+		if ((n >> i) & 1)
+		{
+			_putchar('1');
+			printed = 1;
+		}
+		else if (printed == 1)
+			_putchar('0');
+		i--;
 	}
 }
