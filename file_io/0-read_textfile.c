7dc3d0ca6aa24c074e17210ae404a297467565b3
camtimms <cam.g.timms@gmail.com>
2025-04-10 06:53:37 +0000

    added task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -36,7 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	bytes_read = read(fd, buffer, sizeof(buffer));
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read < 0)
 	{
 		free(buffer);
